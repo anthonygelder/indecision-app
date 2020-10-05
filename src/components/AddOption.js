99acd2602b7032c552f44008c1aeb0183b007854
Anthony Gelder <anthony.gelder@gmail.com>
2020-10-04 22:30:13 -0700

    webpack dev tools and dev server

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -10,7 +10,6 @@ class AddOption extends React.Component {
     }
     handleAddOption(e) {
         e.preventDefault()
-
         const option = e.target.option.value.trim()
         const error = this.props.handleAddOption(option)
 
