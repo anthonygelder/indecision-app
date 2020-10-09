7964c1357e2943f6785601b58c991382f326d8be
Anthony Gelder <anthony.gelder@gmail.com>
2020-10-08 21:20:16 -0700

    'style'

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,17 +1,15 @@
 import React from 'react'
 
-const Option = (props) => {
-    return (
-        <div>
-            <p>{props.optionText}</p>
-            <button 
-                onClick={(e) => {
-                    props.handleDeleteOption(props.optionText)
-                }}>
-                Delete
-            </button>
-        </div>
-    )
-}
+const Option = (props) => (
+    <div>
+        <p>{props.optionText}</p>
+        <button 
+            onClick={(e) => {
+                props.handleDeleteOption(props.optionText)
+            }}>
+            Delete
+        </button>
+    </div>
+)
 
 export default Option
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import Option from './Option'
 
-const Options = (props) => {
-    return (
-        <div>
-            {props.options.length === 0 && <p>Add an option</p>}
-            {props.options.map((option) => <Option handleDeleteOption={props.handleDeleteOption} key={option} optionText={option}/>)}
-            <button onClick={props.handleDeleteOptions}>Remove all</button>
-        </div>
-    )
-}
+const Options = (props) => (
+    <div>
+        {props.options.length === 0 && <p>Add an option</p>}
+        {props.options.map((option) => <Option handleDeleteOption={props.handleDeleteOption} key={option} optionText={option}/>)}
+        <button onClick={props.handleDeleteOptions}>Remove all</button>
+    </div>
+)
 
 export default Options
