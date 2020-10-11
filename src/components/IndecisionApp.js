cd722787aa0195d5163eed17418345aee54e88fb
Anthony Gelder <anthony.gelder@gmail.com>
2020-10-10 22:01:50 -0700

    scss styles

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -70,13 +70,15 @@ class IndecisionApp extends React.Component {
         return (
             <div>
                 <Header subtitle={subtitle}/>
-                <Action hasOptios={this.state.options.length > 0} 
-                        handlePick={this.handlePick} />
-                <Options options={this.state.options}
-                    handleDeleteOptions={this.handleDeleteOptions}
-                    handleDeleteOption={this.handleDeleteOption}
-                />
-                <AddOption handleAddOption={this.handleAddOption} />
+                <div className="container">
+                    <Action hasOptios={this.state.options.length > 0} 
+                            handlePick={this.handlePick} />
+                    <Options options={this.state.options}
+                        handleDeleteOptions={this.handleDeleteOptions}
+                        handleDeleteOption={this.handleDeleteOption}
+                    />
+                    <AddOption handleAddOption={this.handleAddOption} />
+                </div>
                 <OptionModal 
                     selectedOption={this.state.selectedOption}  
                     clearSelectedOption={this.clearSelectedOption}  
