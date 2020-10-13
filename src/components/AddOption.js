5d22212ab6806084c426f8ec83856228034c1a83
Anthony Gelder <anthony.gelder@gmail.com>
2020-10-12 21:51:03 -0700

    final scss

--- src/components/AddOption.js
+++ src/components/AddOption.js
@@ -22,11 +22,11 @@ class AddOption extends React.Component {
     render() {
         return (
             <div>
-            <form onSubmit={this.handleAddOption}>  
-                <input type='text' name="option"></input>
+            <form className="add-option" onSubmit={this.handleAddOption}>  
+                <input className="add-option__input" type='text' name="option"></input>
             <button className="button">Add option</button>
             </form>
-            {this.state.error && <p>{this.state.error}</p>}
+            {this.state.error && <p className="add-option-error">{this.state.error}</p>}
             </div>
         )
     }
--- src/components/Options.js
+++ src/components/Options.js
@@ -8,7 +8,15 @@ const Options = (props) => (
             <button className="button button--link" onClick={props.handleDeleteOptions}>Remove all</button>
         </div>
         {props.options.length === 0 && <p className="widget--message">Please add an option</p>}
-        {props.options.map((option) => <Option handleDeleteOption={props.handleDeleteOption} key={option} optionText={option}/>)}
+        {props.options.map((option, index) => (
+            <Option 
+                handleDeleteOption={props.handleDeleteOption} 
+                key={option} 
+                optionText={option}
+                count={index + 1}
+            />
+        ))}
+            
     </div>
 )
 
